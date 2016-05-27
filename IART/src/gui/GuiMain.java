7176a7cd5c21391e234b2e13975627ee9868b710
Rui Vilares <rui.vilares@hotmail.com>
2016-05-27 22:01:41 +0100

    simulated + generic added

--- IART/src/gui/GuiMain.java
+++ IART/src/gui/GuiMain.java
@@ -33,6 +33,7 @@ public class GuiMain extends JFrame{
     private JList list1;
     private JButton geneticAlgorithmButton;
     private JButton simulatedAnnealingButton;
+    private JButton geneticAlgorithmSimulatedAnnealingButton;
     private JScrollBar scrollBar1;
     private JLabel label;
 
@@ -300,6 +301,140 @@ public class GuiMain extends JFrame{
 
                                 try {
                                     showWindow("Simulated Annealing" + " - " + modeList.getSelectedItem(), "simulatedAnnealing.html");
+                                } catch (IOException e1) {
+                                    e1.printStackTrace();
+                                }
+                                long endTime   = System.currentTimeMillis();
+                                long totalTime = endTime - startTime;
+                                someFrame.setVisible(false);
+                                JOptionPane.showMessageDialog(GuiMain.this, "Score: " + best + "\nRunning time: " + totalTime + " ms", "Done", JOptionPane.INFORMATION_MESSAGE);
+                            }
+                            catch (NumberFormatException nfe){
+                                JOptionPane.showMessageDialog(GuiMain.this, "Enter valid values", "Error", JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                    });
+                }
+                else {
+                    JOptionPane.showMessageDialog(GuiMain.this, "You need load cities", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+
+        geneticAlgorithmSimulatedAnnealingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (parser.getCities().size() > 0) {
+                    JFrame main = new JFrame("Genetic Algorithm + Simulated Annealing");
+                    main.setSize(300, 400);
+                    main.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+                    String[] modeString = { "Nr of Courts", "Price"};
+                    JComboBox modeList = new JComboBox(modeString);
+                    modeList.setSelectedIndex(0);
+                    JTextField variable = new JTextField("50", 2);
+
+                    JTextField bestToPass = new JTextField("5", 2);
+                    JTextField generationSize = new JTextField("20", 2);
+                    JTextField iterations = new JTextField("500", 2);
+                    JTextField pbMutation = new JTextField("25", 2);
+                    JTextField pbMarriage = new JTextField("75", 2);
+
+                    JTextField initialTemperature = new JTextField("50", 2);
+                    JTextField delta = new JTextField("0.01", 2);
+                    JTextField dist = new JTextField("1", 2);
+                    JTextField iterationsBeforeStop = new JTextField("0.5", 2);
+
+
+                    JPanel gui = new JPanel(new BorderLayout(3,3));
+                    gui.setBorder(new EmptyBorder(5,5,5,5));
+                    main.setContentPane(gui);
+
+                    JPanel labels = new JPanel(new GridLayout(0,1));
+                    JPanel controls = new JPanel(new GridLayout(0,1));
+                    gui.add(labels, BorderLayout.WEST);
+                    gui.add(controls, BorderLayout.CENTER);
+
+                    labels.add(new JLabel("Select mode"));
+                    controls.add(modeList);
+                    labels.add(new JLabel("Variable: "));
+                    controls.add(variable);
+
+                    labels.add(new JLabel("Best to pass: "));
+                    controls.add(bestToPass);
+                    labels.add(new JLabel("Generation size: "));
+                    controls.add(generationSize);
+                    labels.add(new JLabel("Iterations: "));
+                    controls.add(iterations);
+                    labels.add(new JLabel("Pb mutation: "));
+                    controls.add(pbMutation);
+                    labels.add(new JLabel("Pb marriage: "));
+                    controls.add(pbMarriage);
+
+                    labels.add(new JLabel("Initial temperature: "));
+                    controls.add(initialTemperature);
+                    labels.add(new JLabel("Delta: "));
+                    controls.add(delta);
+                    labels.add(new JLabel("Dist: "));
+                    controls.add(dist);
+                    labels.add(new JLabel("Iterations before stop: "));
+                    controls.add(iterationsBeforeStop);
+
+                    JButton submit = new JButton("Submit");
+
+                    gui.add(submit, BorderLayout.SOUTH);
+                    main.pack();
+                    main.setVisible(true);
+
+                    submit.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                JFrame someFrame = new JFrame("Running...");
+                                someFrame.setSize(250, 50);
+                                someFrame.setVisible(true);
+                                main.setVisible(false);
+                                long startTime = System.currentTimeMillis();
+
+                                int bp = Integer.parseInt(bestToPass.getText());
+                                int gs = Integer.parseInt(generationSize.getText());
+                                int i = Integer.parseInt(iterations.getText());
+                                int pm = Integer.parseInt(pbMutation.getText());
+                                int pma = Integer.parseInt(pbMarriage.getText());
+
+                                double it = Double.parseDouble(initialTemperature.getText());
+                                double del = Double.parseDouble(delta.getText());
+                                double d = Double.parseDouble(dist.getText());
+                                double ibs = Double.parseDouble(iterationsBeforeStop.getText());
+
+                                Vector<Place> places = parser.getCities();
+
+                                GeneticAlgorithm ga;
+                                SimulatedAnnealing sa;
+
+                                if (modeList.getSelectedIndex() == 0) {
+                                    int var = Integer.parseInt(variable.getText());
+                                    ga = new GeneticAlgorithm(places, var, bp, gs, i, d, pm, pma, ibs);
+                                    places = ga.getBestChoice();
+                                    sa = new SimulatedAnnealing(places, var, it, del, d, ibs);
+                                }
+                                else {
+                                    double var = Double.parseDouble(variable.getText());
+                                    ga = new GeneticAlgorithm(places, var, bp, gs, i, d, pm, pma, ibs);
+                                    places = ga.getBestChoice();
+                                    sa = new SimulatedAnnealing(places, var, it, del, d, ibs);
+                                }
+
+                                sa.compute();
+
+                                int best = sa.getBestScore();
+                                places = sa.getBestChoice();
+
+                                parser.javascriptFileConstructor(places, "simulatedAnnealingGeneticAlgorithm.html");
+
+                                try {
+                                    showWindow("Simulated Annealing + Genetic Algorithm" + " - " + modeList.getSelectedItem(), "simulatedAnnealingGeneticAlgorithm.html");
                                 } catch (IOException e1) {
                                     e1.printStackTrace();
                                 }
